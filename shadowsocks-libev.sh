34165e4c3c410077840f6f83b46ed11fdcc87ef5
LoThaRQ <47138290+LoThaRQ@users.noreply.github.com>
2020-02-23 13:39:12 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -6,7 +6,8 @@ export PATH
 cur_dir=`pwd`
 
 libsodium_file="libsodium-stable"
-libsodium_url="https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
+libsodium_url="http://distfiles.macports.org/libsodium/libsodium-1.0.18-RELEASE.tar.gz"
+# libsodium_url="https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 
 mbedtls_file="mbedtls-2.16.5"
 mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.5-apache.tgz"
